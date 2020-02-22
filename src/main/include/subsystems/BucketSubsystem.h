10f940198e1469e920f1f10a773749a1efbd83e9
Kellen Watt <kbw6d9@mst.edu>
2020-02-22 10:32:19 -0600

    Create BucketSubsystem

---
+++ src/main/include/subsystems/BucketSubsystem.h
@@ -0,0 +1,11 @@
+#ifndef _2165_BUCKETSUBSYSTEM_H_
+#define _2165_BUCKETSUBSYSTEM_H_
+
+#include <frc2/command/SubsystemBase.h>
+
+
+class BucketSubsystem : public frc2::SubsystemBase {
+
+}
+
+#endif
